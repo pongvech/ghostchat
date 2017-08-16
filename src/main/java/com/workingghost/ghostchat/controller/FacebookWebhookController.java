5879f0b1a2d3b9b852d6ee97f2cff620028c5b1e
pongvech(Pun) <pun@scoutout.net>
2017-08-16 20:49:49 +0700

    Another test

--- src/main/java/com/workingghost/ghostchat/controller/FacebookWebhookController.java
+++ src/main/java/com/workingghost/ghostchat/controller/FacebookWebhookController.java
@@ -7,13 +7,19 @@ import com.replyyes.facebook.messenger.bean.FacebookMessengerSendException;
 import com.replyyes.facebook.messenger.bean.Messaging;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class FacebookWebhookController {
 
     private static final Logger logger = LoggerFactory.getLogger(FacebookWebhookController.class);
-    String flyingZeppelinToken = "EAAEhgfZBZB910BABQMJB1mz53s5L0dLoTboOQYxsW639aeFNZAQu6M6oDzCiwYyyAb6GvQPhpA5JFdOqgkpCjDgKfXd7xZBrzg3CaYL8CwEQiUOwvBpKzd2odwaD4tQd7cVl6VlT0xuNKNvJEaRZB8Cxn1PeXVZCiqfkX4rX0IhgZDZD";
+
+    @Value("${facebook_page_token}")
+    private String token;
+    @Value("${facebook_app_secret_key}")
+    private String secretKey;
 
     @RequestMapping(path = "/webhook", method = RequestMethod.GET)
     public String defaultWebhook(@RequestParam(value = "hub.challenge") String challenge){
@@ -22,45 +28,59 @@ public class FacebookWebhookController {
         return challenge;
     }
 
+    @RequestMapping(path = "/getstartbutton", method = RequestMethod.POST)
+    public String setGetStartButton(@RequestBody String request) {
+        RestTemplate restTemplate = new RestTemplate();
+        logger.debug("Create GetStarted with {}", request);
+        return restTemplate.postForObject("https://graph.facebook.com/v2.6/me/messenger_profile?access_token="+token,
+                request, String.class);
+    }
+
+    @RequestMapping(path = "/persistentmenu", method = RequestMethod.POST)
+    public String setPersistentMenu(@RequestBody String request) {
+        RestTemplate restTemplate = new RestTemplate();
+        logger.debug("Create Persistent Menu with {}", request);
+        return restTemplate.postForObject("https://graph.facebook.com/v2.6/me/messenger_profile?access_token="+token,
+                request, String.class);
+    }
+
     @RequestMapping(path = "/webhook", method = RequestMethod.POST)
-    public String postWebhook(@RequestBody String request){
+    public String postWebhook(@RequestBody String request,
+                              @RequestHeader("X-Hub-Signature") String signature) throws FacebookMessengerSendException {
         logger.debug("Got webhook request!");
+        logger.debug("X-Hub-Signature = {}", signature);
         logger.debug(request);
 
         FacebookMessengerClient client = new FacebookMessengerClient();
+        if (client.isValidRequest(secretKey, signature, request)) {
+            logger.debug("Request is valid!");
+        } else {
+            logger.error("Request is invalid!");
+            return null;
+        }
+
         Callback callback = client.deserializeCallback(request);
-        String senderId = null;
-        String replyText = null;
+        String senderId;
+        String replyText;
 
         logger.debug("Object = {}", callback.getObject());
         logger.debug("Found {} entry elements", callback.getEntry().size());
         for (Entry entry : callback.getEntry()) {
-            logger.debug("id = {}", entry.getId());
-            logger.debug("time = {}", entry.getTime());
             logger.debug("Found {} messaging elements", entry.getMessaging().size());
             for (Messaging messaging : entry.getMessaging()) {
-                logger.debug("sender = {}", messaging.getSender().getId());
-                logger.debug("recipient = {}", messaging.getRecipient().getId());
-                logger.debug("timestamp = {}", messaging.getTimestamp());
-                logger.debug("mid = {}", messaging.getMessage().getMid());
-                logger.debug("sec = {}", messaging.getMessage().getSeq());
-                logger.debug("text = {}", messaging.getMessage().getText());
                 senderId = messaging.getSender().getId();
-                replyText = messaging.getMessage().getText();
-            }
-        }
-        if (senderId != null && replyText != null) {
-            try {
-                if (replyText.equals("parn")) {
-                    client.sendTextMessage(flyingZeppelinToken, senderId, "I love you :)");
+                if (messaging.getMessage() != null) {
+                    replyText = messaging.getMessage().getText();
+                    if (replyText.equals("parn")) {
+                        client.sendTextMessage(token, senderId, "I love you babe!!! :)");
+                    } else {
+                        client.sendTextMessage(token, senderId, "You said \""+replyText+"\"? :)");
+                    }
+                } else if (messaging.getPostback() != null && messaging.getPostback().getPayload().equals("GET_STARTED_PAYLOAD")) {
+                    replyText = "Greeting! Welcome to Flying Zeppelin!";
+                    client.sendTextMessage(token, senderId, ""+replyText);
                 }
-                client.sendTextMessage(flyingZeppelinToken, senderId, replyText);
-            } catch (FacebookMessengerSendException e) {
-                logger.error("Something went wrong!!");
-                e.printStackTrace();
             }
-        } else {
-            logger.error("Either senderId or replyText is NULL!!!");
         }
         return request;
     }
