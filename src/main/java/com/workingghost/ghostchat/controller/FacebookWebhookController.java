6e02c80beeb3c1c293f5fdda0b09e81dabc7a872
Pongvech Vechprasit (Pun) <pun@scoutout.net>
2017-08-17 14:16:58 +0700

    Added more feature

--- src/main/java/com/workingghost/ghostchat/controller/FacebookWebhookController.java
+++ src/main/java/com/workingghost/ghostchat/controller/FacebookWebhookController.java
@@ -2,14 +2,19 @@ package com.workingghost.ghostchat.controller;
 
 import com.replyyes.facebook.messenger.FacebookMessengerClient;
 import com.replyyes.facebook.messenger.bean.Callback;
+import com.replyyes.facebook.messenger.bean.Element;
 import com.replyyes.facebook.messenger.bean.Entry;
 import com.replyyes.facebook.messenger.bean.FacebookMessengerSendException;
 import com.replyyes.facebook.messenger.bean.Messaging;
+import com.replyyes.facebook.messenger.bean.QuickReply;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class FacebookWebhookController {
@@ -71,10 +76,43 @@ public class FacebookWebhookController {
                 senderId = messaging.getSender().getId();
                 if (messaging.getMessage() != null) {
                     replyText = messaging.getMessage().getText();
-                    if (replyText.equals("parn")) {
+                    if (replyText.equalsIgnoreCase("parn")) {
                         client.sendTextMessage(token, senderId, "I love you babe!!! :)");
+                    } else if (replyText.equalsIgnoreCase("generics")) {
+                        List<Element> elementList = new ArrayList<>();
+                        Element flyingZeppelin = new Element();
+
+                        flyingZeppelin.setTitle("What is Flying Zeppelin?");
+                        flyingZeppelin.setSubtitle("A Zeppelin was a type of rigid airship named after the German Count " +
+                                "Ferdinand von Zeppelin (German pronunciation: [ˈt͡sɛpəliːn]) who pioneered rigid " +
+                                "airship development at the beginning of the 20th century.");
+                        flyingZeppelin.setImageUrl("https://s3-ap-southeast-1.amazonaws.com/fzpublic/fz1.jpg");
+                        flyingZeppelin.setItemUrl("https://en.wikipedia.org/wiki/Zeppelin");
+
+                        Element design = new Element();
+                        design.setTitle("Design History");
+                        design.setSubtitle("Count Ferdinand von Zeppelin's serious interest in airship development " +
+                                "began in 1874, when he took inspiration from a lecture given by Heinrich von Stephan " +
+                                "on the subject of \"World Postal Services and Air Travel\" to outline the basic " +
+                                "principle of his later craft in a diary entry dated 25 March 1874.");
+                        design.setImageUrl("https://s3-ap-southeast-1.amazonaws.com/fzpublic/fz2.jpg");
+                        design.setItemUrl("https://en.wikipedia.org/wiki/Zeppelin#History");
+
+                        List<QuickReply> quickReplieList = new ArrayList<>();
+                        QuickReply quickReply1 = new QuickReply();
+                        quickReply1.setTitle("Quick reply1");
+                        quickReply1.setPayload("QUICK_1");
+                        QuickReply quickReply2 = new QuickReply();
+                        quickReply2.setTitle("Quick reply2");
+                        quickReply2.setPayload("QUICK_2");
+                        quickReplieList.add(quickReply1);
+                        quickReplieList.add(quickReply2);
+
+                        elementList.add(flyingZeppelin);
+                        elementList.add(design);
+                        client.sendGenericMessage(token, senderId, elementList, quickReplieList);
                     } else {
-                        client.sendTextMessage(token, senderId, "You said \""+replyText+"\"? :)");
+                        client.sendTextMessage(token, senderId, "You said \""+replyText+"\"? :) Hint: try \"generics\"");
                     }
                 } else if (messaging.getPostback() != null && messaging.getPostback().getPayload().equals("GET_STARTED_PAYLOAD")) {
                     replyText = "Greeting! Welcome to Flying Zeppelin!";
